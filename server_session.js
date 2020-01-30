2fe7c6d04cb2131a824335a856efb97ee9d809ee
Philippe DEKIMPE <dkp@ecam.be>
2020-01-30 16:39:43 +0100

    add cookie and session

---
+++ server_session.js
@@ -0,0 +1,44 @@
+// Import express
+let express = require('express');
+
+// Initialize the app
+let app = express();
+
+// Setup server port
+var port = process.env.PORT || 8080;
+
+let session = require('express-session');
+app.use(session({
+	secret: 'my secret',
+	resave: false,
+	saveUninitialized: true
+	})
+);
+
+// Login
+app.get('/login/:user', (req, res) => {
+	req.session.user = req.params.user ;
+	console.log(req.session);
+	res.send('Hello ' + req.session.user);
+});
+
+// Show user session
+app.get('/test', (req, res) => {
+    console.log(req.session);
+    res.send('hello session ' + req.session.user);
+    });
+
+// Logout endpoint
+app.get('/logout', function (req, res) {
+	req.session.destroy();
+	res.send("Logout success! ");
+});
+
+
+// Send message for default URL
+app.get('/', (req, res) => res.send('Hello World !'));
+
+// Launch app to listen to specified port
+app.listen(port, function () {
+    console.log("Runnings on port " + port);
+});
