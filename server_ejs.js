b32a953fa0b949bdacb15c3bb4bc64ed831fbbc2
Philippe DEKIMPE <dkp@ecam.be>
2020-01-30 15:54:41 +0100

    add message

--- server_ejs.js
+++ server_ejs.js
@@ -14,7 +14,7 @@ app.get('/', function (req, res) {
 })
 
 // Use template
-app.get('/compter/:nombre', function(req, res) {
+app.get('/compter/:nombre?', function(req, res) {
     var noms = ['Robert', 'Jacques', 'David'];
     res.render('page.ejs', {compteur: req.params.nombre, noms: noms});
 
