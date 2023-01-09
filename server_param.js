c295a5fb919d95157a6acb5f9a9bc40025fbbd8f
clarophil <dkp@ecam.be>
2023-01-09 14:28:56 +0100

    enhance params

--- server_param.js
+++ server_param.js
@@ -7,18 +7,23 @@ let app = express();
 // Setup server port
 var port = process.env.PORT || 8000;
 
+// Send message for default URL
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World !</h1><br><a href="/index?name=toto" >Query request</a></h1><br><a href="/index/name/toto" >Query request</a>');
+});
+
 // using query
 app.get('/index', (req, res) => 
 {
     console.log( req.query.name);
-    res.send('Bonjour ' + req.query.name );
+    res.send('Hello ' + req.query.name );
 });
 
 // using params
 app.get('/index/name/:name', (req, res) => 
 {
     console.log( req.params.name);
-    res.send('Bonjour ' + req.params.name);
+    res.send('Hello ' + req.params.name);
 });
 
 // Launch app to listen to specified port
