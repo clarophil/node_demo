46d37d49f86a8875ae90412e371b34d7cdd46558
clarophil <dkp@ecam.be>
2023-01-09 11:11:42 +0100

    add static and html

---
+++ server.js
@@ -0,0 +1,32 @@
+// Import express
+let express = require('express');
+
+// Initialize the app
+let app = express();
+
+// Setup server port
+var port = 8000;
+
+// Send message for default URL
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World !</h1>');
+});
+
+//setting static files
+app.use(express.static('public'));
+
+// Send message for default URL
+app.get('/style', (req, res) => {
+    res.sendFile('views/test.html', { root: __dirname })
+});
+
+// Manage bad route
+app.use(function (req, res, next) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.status(404).send('Page introuvable !');
+});
+
+// Launch app to listen to specified port
+app.listen(port, function () {
+    console.log("Runnings on port " + port);
+});
